2468067a1c997fc5a6e2256d298e73a8b3badba2
Sonda <genoino.jr@Gmail.com>
2022-05-23 21:56:25 -0300

    Primeiro middleware
    Middleware para validação do usuário.

--- fundamentos-nodejs/index.js
+++ fundamentos-nodejs/index.js
@@ -6,6 +6,25 @@ app.use(express.json());
 
 const customers = [];
 
+// MIDDLEWARE
+
+function verifyIfExistsAccountCPF(req, res, next) {
+
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(400).json({ Error: "Customer not found!" });
+    }
+
+    req.customer = customer;
+
+    return next();
+
+}
+
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
@@ -25,15 +44,9 @@ app.post("/account", (req, res) => {
     return res.status(200).json({ customers });
 })
 
-app.get("/statement", (req, res) => {
-    const { cpf } = req.headers;
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
 
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if (!customer) {
-        return res.status(400).json({ Error: "Customer not found!" });
-    }
-
+    const { customer } = req;
     return res.json(customer);
 
 })
